57f0408cca77be468273b908325ec11e88437cb5
박강원 <nambaggangwon@gmail.com>
2023-12-18 08:09:59 +0900

    주석 추가

--- 정렬/버블정렬.c
+++ 정렬/버블정렬.c
@@ -4,7 +4,7 @@ int main(){
 	int key, min, num;
 	int n = sizeof(list) / sizeof(int);
 
-	for(int i=n-1; i>0; i--){
+	for(int i=n-1; i>0; i--){//오름차순으로 정렬하기 때문에 가장 큰 값은 한 번 정렬될 때마다 제일 우측에 있으므로 굳이 포함시켜서 정렬시킬 필요없으므로 i--해줌
 		for(int j=0; j<i; j++){
 			if(list[j] > list[j+1]){
 				num = list[j+1];
--- 정렬/선택정렬.c
+++ 정렬/선택정렬.c
@@ -5,10 +5,13 @@ int main(){
 	int n = sizeof(list) / sizeof(int);
 	
 	for(int i=0; i<n-1; i++){ 
-		min = i;
+		min = i;// 현재까지의 최솟값의 인덱스를 저장하는 변수 min을 초기화
 		for(int j=i+1; j<n; j++){
-			if(list[j] < list[min]) min = j;
+			// 현재까지의 최솟값보다 작은 값을 찾으면 min을 업데이트
+			if(list[j] < list[min]) 
+				min = j;
 		}
+		// i번째 위치의 값과 최솟값(min) 위치의 값을 교환합니다.
 		num = list[i];
 		list[i] = list[min];
 		list[min] = num;
