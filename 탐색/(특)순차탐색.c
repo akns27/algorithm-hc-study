e6098ddc7f6ae26fb07f64d6aabbcecf40c7ef34
박강원 <nambaggangwon@gmail.com>
2023-12-18 11:15:30 +0900

    이름&순차탐색 주석

--- 탐색/(특)순차탐색.c
+++ 탐색/(특)순차탐색.c
@@ -4,9 +4,11 @@ int key, result;
 
 int search(int low, int high){
 	for(int i=low; i<high; i++){
+		//처음부터 끝까지 1씩 증가시켜가며 key값과 똑같은 list값이 있다면 그 값보다 +1된 값 반환, 
+		//배열이 0부터 시작하기에 실제로 비교한 횟수는 i+1이 되야함
 		if(list[i] == key) return i+1;
 	}
-	return -1; 
+	return -1; //아니면 -1반환
 }
 
 int main(){
